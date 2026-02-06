97ebb46138c1f717ce1581eda5772d4781efd991
Mohammad Ashik <star.supplierss@gmail.com>
2026-02-06 16:27:51 +0530

    patch code

--- src/common/index.db.js
+++ src/common/index.db.js
@@ -1,31 +1,26 @@
- const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
+const sequelize = require('./db.config');
 
- const sequelize = require('./db.config');
+// Models Import
+const User = require('../modules/auth/models/user.model');
+const EmployeeMaster = require('../modules/hrm/model/EmployeeMaster');
+const CheckIn = require('../modules/hrm/model/CheckIn_model');
+const CheckOut = require('../modules/hrm/model/CheckOut_model');
+const OfficeLocation = require('../modules/hrm/model/Office_Location_model');
 
-
-//  improt models
-    const User = require('../modules/auth/models/user.model');
-    const EmployeeMaster = require('../modules/hrm/model/EmployeeMaster');
-    const CheckIn = require('../modules/hrm/model/CheckIn_model'); 
-    const CheckOut = require('../modules/hrm/model/CheckOut_model');
-    const OfficeLocation = require('../modules/hrm/model/Office_Location_model');
-
-    
-
-
-
-    const db = {
-    sequelize, 
-    Sequelize, 
-    User,      
+const db = {
+    sequelize,
+    Sequelize,
+    User,
     EmployeeMaster,
     CheckIn,
     CheckOut,
-    OfficeLocation    
-    };
+    OfficeLocation
+};
 
-    // Associations can be defined here
-   // Employee to CheckIn (One-to-Many)
+// --- ASSOCIATIONS (RELATIONS) ---
+
+// 1. Employee to CheckIn (One-to-Many)
 db.EmployeeMaster.hasMany(db.CheckIn, { 
     foreignKey: 'employee_master_id', 
     as: 'checkins' 
@@ -35,41 +30,45 @@ db.CheckIn.belongsTo(db.EmployeeMaster, {
     as: 'employee'
 });
 
-// Employee to CheckOut (One-to-Many)
+// 2. Employee to CheckOut (One-to-Many)
 db.EmployeeMaster.hasMany(db.CheckOut, { 
     foreignKey: 'employee_master_id', 
-    sourceKey: 'id',        
     as: 'checkouts' 
 });
-
 db.CheckOut.belongsTo(db.EmployeeMaster, { 
     foreignKey: 'employee_master_id', 
-    targetKey: 'id', 
     as: 'employee'
 });
 
-// Ek location ke andar bahut saare employees ho sakte hain
+// 3. OfficeLocation to EmployeeMaster (Geofencing ke liye)
 db.OfficeLocation.hasMany(db.EmployeeMaster, { 
     foreignKey: 'location_id', 
     as: 'employees' 
 });
-
-// Ek employee ki sirf EK hi location hogi
 db.EmployeeMaster.belongsTo(db.OfficeLocation, { 
     foreignKey: 'location_id', 
     as: 'location' 
 });
 
+// 4. User to EmployeeMaster (Login Mapping)
+db.User.hasOne(db.EmployeeMaster, { 
+    foreignKey: 'userId', // Check karein aapke DB mein 'userId' hai ya 'user_id'
+    as: 'employeeProfile'
+});
 db.EmployeeMaster.belongsTo(db.User, { 
     foreignKey: 'userId', 
     as: 'loginDetails' 
 });
 
-// Reverse relation (User ke paas ek employee profile hai)
-db.User.hasOne(db.EmployeeMaster, { 
-    foreignKey: 'userId',
-    as: 'employeeProfile'
+// 5. Self-Referencing (Supervisor to Team Members)
+// Yeh line aapka "Team" render karne mein madad karegi
+db.EmployeeMaster.hasMany(db.EmployeeMaster, {
+    foreignKey: 'reporting_manager_id',
+    as: 'teamMembers'
+});
+db.EmployeeMaster.belongsTo(db.EmployeeMaster, {
+    foreignKey: 'reporting_manager_id',
+    as: 'supervisor'
 });
 
-
-module.exports = db
+module.exports = db;
--- src/modules/hrm/model/EmployeeMaster.js
+++ src/modules/hrm/model/EmployeeMaster.js
@@ -1,79 +1,74 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../../../common/db.config');
-const { de } = require('zod/locales');
-const { OfficeLocation } = require('../../../common/index.db');
-const EmployeeMaster = sequelize.define('EmployeeMaster', {
-  
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-    field: 'employee_master_id'
-  },
-  emp_code: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  address:{
-    type: DataTypes.STRING,
-    allowNull: false
+  const { DataTypes } = require('sequelize');
+  const sequelize = require('../../../common/db.config');
+  const { de } = require('zod/locales');
+  const EmployeeMaster = sequelize.define('EmployeeMaster', {
+    
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      field: 'employee_master_id'
     },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,    
-    validate: { isEmail: true }
-  },
-  department: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  position: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  // role: {
-  //   type: DataTypes.ENUM('Admin', 'Sales', 'Employee', 'Site Suppervisor', 'Technical Team'),
-  //   defaultValue: 'employee'
-  // },
-  monthly_wages: {
-    type: DataTypes.DECIMAL(10, 2),
-    allowNull: false
-  },
-  location_id: {
-    type: DataTypes.UUID,
-    allowNull: true,
-  },
-  isActive: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true 
-  },
-  supervisor_id: {
-        type: DataTypes.UUID,
-        allowNull: true, // Top level admin ke liye null hoga
-        references: {
-            model: 'hrm_employee_master',
-            key: 'employee_master_id'
-        }
+    emp_code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
     },
-  user_id: {
-    type: DataTypes.UUID,
-    allowNull: true,
-    field: 'userId'
-  }
-}, {
-  tableName: 'hrm_employee_master', 
-  timestamps: true 
-});
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    address:{
+      type: DataTypes.STRING,
+      allowNull: false
+      },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,    
+      validate: { isEmail: true }
+    },
+    department: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    position: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    monthly_wages: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    location_id: {
+      type: DataTypes.UUID,
+      allowNull: true,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true 
+    },
+    supervisor_id: {
+          type: DataTypes.UUID,
+          allowNull: true, // Top level admin ke liye null hoga
+          references: {
+              model: 'hrm_employee_master',
+              key: 'employee_master_id'
+          }
+      },
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      field: 'userId'
+    }
+  }, {
+    tableName: 'hrm_employee_master', 
+    timestamps: true 
+  });
 
-module.exports = EmployeeMaster;
+  module.exports = EmployeeMaster;
