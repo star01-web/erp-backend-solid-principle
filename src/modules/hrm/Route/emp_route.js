96084327faa7c0c3ac0f676119ff14aca35ffc9e
Mohammad Ashik <star.supplierss@gmail.com>
2026-05-09 12:09:59 +0530

    conflict solution

--- src/modules/hrm/Route/emp_route.js
+++ src/modules/hrm/Route/emp_route.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {CreateEmployee,updateEmployee,getallEmployee, getEmployeeProfile } = require('../Controller/employee_controller.js');
+const {
+  CreateEmployee,
+  updateEmployee,
+  getallEmployee,
+  getEmployeeProfile,
+  bulkCreateEmployees,
+} = require("../Controller/employee_controller.js");
 
 const asyncHandler = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
+  Promise.resolve(fn(req, res, next)).catch(next);
 };
 
-router.post('/create-employee', asyncHandler(CreateEmployee));
-router.put('/update-employee/:id', asyncHandler(updateEmployee));
-router.get('/get-all-employees', asyncHandler(getallEmployee));
-router.get('/get-user-profile', asyncHandler(getEmployeeProfile));
+router.post("/create-employee", asyncHandler(CreateEmployee));
+router.post("/create-bulk-employee", asyncHandler(bulkCreateEmployees));
+router.put("/update-employee/:id", asyncHandler(updateEmployee));
+router.get("/get-all-employees", asyncHandler(getallEmployee));
+router.get("/get-user-profile", asyncHandler(getEmployeeProfile));
 module.exports = router;
