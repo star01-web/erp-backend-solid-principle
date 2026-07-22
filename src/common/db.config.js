58f12fd7bb50b9204160728a37a449d38428d7f2
Mohammad Ashik <star.supplierss@gmail.com>
2026-07-22 17:02:10 +0530

    .env chaganges

--- src/common/db.config.js
+++ src/common/db.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env"), override: true });
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
